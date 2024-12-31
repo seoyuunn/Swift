ac34aab2cf6d1157e2d85e5f9e9f77ae14bdec35
Yunseo <100517496+seoyuunn@users.noreply.github.com>
2024-12-31 17:58:33 +0900

    Add files via upload

---
+++ Basics/03_project2/Hello,iOS/Hello,iOS/ViewController.swift
@@ -0,0 +1,19 @@
+ //
+//  ViewController.swift
+//  Hello,iOS
+//
+//  Created by 김윤서 on 12/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Basics/03_project2/Hello,iOS/Hello,iOSTests/Hello_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_iOSTests.swift
+//  Hello,iOSTests
+//
+//  Created by 김윤서 on 12/31/24.
+//
+
+import Testing
+@testable import Hello_iOS
+
+struct Hello_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

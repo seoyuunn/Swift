2ae874c3f3a98140ee4c8f4c416937a180a43954
Yunseo <100517496+seoyuunn@users.noreply.github.com>
2025-02-10 21:19:10 +0900

    Add files via upload
    스토리보드에서 UI 구현 중에 코드를 실행하면 버튼 크기가 변경되면서 비율이 깨지는 문제 발생.

---
+++ calculator02/calculator02/calculator02/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  calculator02
+//
+//  Created by 김윤서 on 2/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ calculator02/calculator02/calculator02Tests/calculator02Tests.swift
@@ -0,0 +1,17 @@
+//
+//  calculator02Tests.swift
+//  calculator02Tests
+//
+//  Created by 김윤서 on 2/9/25.
+//
+
+import Testing
+@testable import calculator02
+
+struct calculator02Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

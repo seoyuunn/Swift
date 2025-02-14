3e75ae63a33fecf11dce83fa84b206af89b0bdfb
Yunseo Kim <wozoyun@gmail.com>
2025-02-14 17:26:53 +0900

    BasicCalculatorTutorial 프로젝트 생성

---
+++ Calculators/BasicCalculatorTutorial/BasicCalculatorTutorialTests/BasicCalculatorTutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  BasicCalculatorTutorialTests.swift
+//  BasicCalculatorTutorialTests
+//
+//  Created by 김윤서 on 2/14/25.
+//
+
+import Testing
+@testable import BasicCalculatorTutorial
+
+struct BasicCalculatorTutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
